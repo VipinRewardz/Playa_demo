4ebb95a11d74be6c74256616f9cdd4f585240b16
Rewardz <87293928+VipinRewardz@users.noreply.github.com>
2024-09-19 12:30:59 +0530

    logo changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -46,7 +46,11 @@ export default function LoginPage() {
         <div className="flex bg-privy-light-blue flex-1 p-6 justify-center items-center">
           <div>
             <div>
-              <Portal style={{ maxWidth: "100%", height: "auto" }} />
+            <img
+                src="/logos/petfriends.png"
+                alt="Privy Logo"
+                style={{ maxWidth: "100%", height: "auto" }}
+              />
             </div>
             <div className="mt-6 flex justify-center text-center">
               <button
